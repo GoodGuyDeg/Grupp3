0ea50c3abffca8d1025b374032b5531fba5e09ac
Julstil <79506162+Julstil@users.noreply.github.com>
2021-03-16 09:37:44 +0100

    Revert "Revert "Hej""
    This reverts commit fe182bfbd3561e503e637156a9fbf8457d85b882.

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    
+    public void StartGame() //startgame funktion - Robin
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //byter till nästa scen - Robin
+    }
+}
