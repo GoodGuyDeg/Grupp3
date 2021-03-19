700db46a3bb690ac2d4e0b37d8feba1d73764a5a
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2021-03-19 09:55:24 +0100

    Main Menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+  public void LoadLVL1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
