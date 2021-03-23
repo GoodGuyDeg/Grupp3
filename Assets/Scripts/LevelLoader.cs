e028231dc4e0127f7b1a431340c532397cbcbba5
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2021-03-23 10:35:03 +0100

    Paus Meny Level 1
    Paus Meny level 1

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,12 +6,24 @@ public class LevelLoader : MonoBehaviour
 {
   public void LoadLVL1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Ändrar Sceen till Level 1/Timothy
     }
 
+    public void LoadLVL2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); //Ändrar Sceen till Level 2/Timothy
+    }
+    public void LoadLVL3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3); //Ändrar Sceen till Level 3/Timothy
+    }
+    public void LoadStart()
+    {
+        SceneManager.LoadScene("StartScreene"); //Ändrar Sceen till Startscreen/
+    }
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); //Avslutar Spelet/ Timothy
         Debug.Log("Quit");
     }
 }
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject PauseMenu;
+     
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Time.timeScale = 0;
+            PauseMenu.SetActive(true);
+       
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        
+    }
+}
