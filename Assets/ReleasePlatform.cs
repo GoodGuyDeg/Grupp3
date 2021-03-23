18e544926cd4672df1f4e38ecbbcf924ed6e0e2d
GoodGuyDeg <79506119+GoodGuyDeg@users.noreply.github.com>
2021-03-23 10:38:18 +0100

    gjort massvis med roliga saker i level 3

---
+++ Assets/ReleasePlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleasePlatform : MonoBehaviour
+{
+    public GameObject bridge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "playerflame") //Om repet nuddar elden - Robin
+        {
+            bridge.transform.position += new Vector3(0, 10000, 0); //Flyttar iväg repet - Robin
+        }
+    }
+}
